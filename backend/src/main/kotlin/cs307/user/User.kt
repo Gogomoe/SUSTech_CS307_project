dc719f627415429e86905edbd197f573f0ebf981
Gogo <gogo81745.moe@gmail.com>
2020-05-04 17:28:24 +0800

    [fix] fix user.isAuthorizedAwait

--- backend/src/main/kotlin/cs307/user/User.kt
+++ backend/src/main/kotlin/cs307/user/User.kt
@@ -5,7 +5,6 @@ import io.vertx.ext.web.RoutingContext
 import io.vertx.kotlin.core.json.array
 import io.vertx.kotlin.core.json.json
 import io.vertx.kotlin.core.json.jsonObjectOf
-import io.vertx.kotlin.ext.auth.isAuthorizedAwait
 
 data class User(
         val username: String,
@@ -23,7 +22,13 @@ data class UserAuth(
         val roles: List<String>,
         val permissions: List<String>
 ) {
-    suspend fun isAuthorizedAwait(authority: String): Boolean = auth.isAuthorizedAwait(authority)
+    suspend fun isAuthorizedAwait(authority: String): Boolean {
+        return if (authority.startsWith("role:")) {
+            authority.substring(5) in roles
+        } else {
+            authority in permissions
+        }
+    }
 }
 
 fun RoutingContext.getUser(): UserAuth? = this.session().get("user")
